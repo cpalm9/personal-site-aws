b4cfffc005f3fc263eac18f2bd9ef7a1ab363eb6
Chris Palmer <chrispalmer191@gmail.com>
2018-10-28 22:05:51 -0600

    added a bare-bones about me page

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,15 +3,11 @@
   <v-layout row wrap>
     <v-flex xs12>
       <div class="text-xs-center">
-        <v-avatar size="125px">
-          <img
-            class="img-circle elevation-7 mb-1"
-            src=""
-          >
-        </v-avatar>
         <div class="headline">Chris <span style="font-weight:bold">Palmer</span></div>
         <div class="subheading text-xs-center grey--text pt-1 pb-3">Learn a little about me</div>
         <div class="headline text-xs-left">School</div>
+        <div class="text-xs-left">Brigham Young University</div>
+        <div class="text-xs-left">MISM</div>
       </div>
     </v-flex>
   </v-layout>
